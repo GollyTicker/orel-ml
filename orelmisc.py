15cb386784151e77d9c9b0399e0da2e3ea9e8715
cbozzo <cristina.bozzo383@edu.unito.it>
2016-11-28 17:29:35 +0100

    first submission

--- orelmisc.py
+++ orelmisc.py
@@ -14,14 +14,18 @@ mriZ = 176
 d_3d_org = 6443008
 shape_3d_org = [176,208,176,1]
 
-# health-indicator read form target.csv
-y_org = [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+asbool = lambda x: str(int(x) is 1)
 
-def saveCSV(xs,filename):
+def saveCSV(y,filename):
   fname = filename + ".csv"
   f = open(fname,"wb")
   wr = csv.writer(f,delimiter=",")
-  wr.writerow(["ID","PREDICTION"])
-  wr.writerows( [ [i+1,age] for i,age in enumerate(xs) ] )
+  wr.writerow(["ID","Sample","Label","Predicted"])
+
+  for i in range(len(y[:,0])):
+    wr.writerows([ [i*3,i,"gender",asbool(y[i,0])],
+                   [i*3+1,i,"age",asbool(y[i,1])],
+                   [i*3+2,i,"health",asbool(y[i,2])] ])
+  
   f.close()
   return fname
